c261ea61e855685cc7526f0c8f93bb50c9e3211d
Jonny Stoten <jonny@jonnystoten.com>
2019-12-24 17:25:04 +0000

    Day 13 part 2

--- lib/days/13.ex
+++ lib/days/13.ex
@@ -3,7 +3,7 @@ defmodule AdventOfCode.Day13 do
 
   alias AdventOfCode.Intcode.Computer
 
-  defmodule ArcadeScreen do
+  defmodule ArcadeCabinet do
     use GenServer
 
     def start_link() do
@@ -11,7 +11,20 @@ defmodule AdventOfCode.Day13 do
     end
 
     def init(_) do
-      {:ok, %{grid: Map.new(), step: :x, location: nil}}
+      {:ok, %{grid: Map.new(), step: :x, location: nil, score: 0, ball_x: 0, paddle_x: 0}}
+    end
+
+    def handle_cast({:put, -1}, %{step: :x} = state) do
+      {:noreply, %{state | step: :segment_y}}
+    end
+
+    def handle_cast({:put, 0}, %{step: :segment_y} = state) do
+      {:noreply, %{state | step: :segment_score}}
+    end
+
+    def handle_cast({:put, value}, %{step: :segment_score} = state) do
+      render_score(value)
+      {:noreply, %{state | step: :x, location: nil, score: value}}
     end
 
     def handle_cast({:put, value}, %{step: :x} = state) do
@@ -22,9 +35,58 @@ defmodule AdventOfCode.Day13 do
       {:noreply, %{state | step: :type, location: Map.put(location, :y, value)}}
     end
 
-    def handle_cast({:put, value}, %{step: :type, grid: grid, location: location}) do
-      grid = Map.put(grid, {location.x, location.y}, tile_type(value))
-      {:noreply, %{step: :x, location: nil, grid: grid}}
+    def handle_cast({:put, value}, %{step: :type, grid: grid, location: location} = state) do
+      tile = tile_type(value)
+      grid = Map.put(grid, {location.x, location.y}, tile)
+      render(location.x, location.y, tile)
+      state = %{state | step: :x, location: nil, grid: grid}
+
+      state =
+        case tile do
+          :ball ->
+            %{state | ball_x: location.x}
+
+          :horizontal_paddle ->
+            %{state | paddle_x: location.x}
+
+          _ ->
+            state
+        end
+
+      {:noreply, state}
+    end
+
+    def handle_call(:get, from, state) do
+      # slow it down so we can see the game play out
+      Process.send_after(self(), {:tick, from}, 1)
+      {:noreply, state}
+    end
+
+    def handle_info({:tick, pid}, %{ball_x: ball_x, paddle_x: paddle_x} = state) do
+      direction =
+        cond do
+          paddle_x < ball_x -> 1
+          paddle_x > ball_x -> -1
+          paddle_x == ball_x -> 0
+        end
+
+      GenServer.reply(pid, direction)
+      {:noreply, state}
+    end
+
+    defp render(x, y, tile) do
+      IO.ANSI.cursor(y + 2, x + 100)
+      |> IO.write()
+
+      tile_symbol(tile)
+      |> IO.write()
+    end
+
+    defp render_score(score) do
+      IO.ANSI.cursor(10, 150)
+      |> IO.write()
+
+      IO.write("SCORE: #{score}")
     end
 
     defp tile_type(0), do: :empty
@@ -33,11 +95,19 @@ defmodule AdventOfCode.Day13 do
     defp tile_type(3), do: :horizontal_paddle
     defp tile_type(4), do: :ball
 
+    defp tile_symbol(:empty), do: " "
+    defp tile_symbol(:wall), do: "#"
+    defp tile_symbol(:block), do: "+"
+    defp tile_symbol(:horizontal_paddle), do: "="
+    defp tile_symbol(:ball), do: "*"
+
     def handle_call(:stop, _from, state) do
-      state.grid
-      |> Map.values()
-      |> Enum.count(&(&1 == :block))
-      |> IO.puts()
+      block_count =
+        state.grid
+        |> Map.values()
+        |> Enum.count(&(&1 == :block))
+
+      IO.puts("Blocks: #{block_count}")
 
       {:stop, :normal, :ok, state}
     end
@@ -54,12 +124,17 @@ defmodule AdventOfCode.Day13 do
   end
 
   def part1(%{memory: memory}) do
-    {:ok, output_pid} = ArcadeScreen.start_link()
+    {:ok, pid} = ArcadeCabinet.start_link()
 
-    Computer.new(memory, nil, output_pid)
+    Computer.new(memory, nil, pid)
     |> Computer.run_to_completion()
   end
 
-  def part2(%{memory: _memory}) do
+  def part2(%{memory: memory}) do
+    {:ok, pid} = ArcadeCabinet.start_link()
+
+    Computer.new(memory, pid, pid)
+    |> Computer.set_memory(0, 2)
+    |> Computer.run_to_completion()
   end
 end
